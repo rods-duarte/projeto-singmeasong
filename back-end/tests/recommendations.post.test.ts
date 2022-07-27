93600ba1eada8fc39e21d98dfc230174ff052851
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-07-27 01:24:40 -0300

    Feat: add post/recommendations/:id/upvote integration tests

--- back-end/tests/recommendations.post.test.ts
+++ back-end/tests/recommendations.post.test.ts
@@ -57,6 +57,22 @@ describe('create music recommendation tests', () => {
     expect(recommendationCreated).toBe(null);
   });
 
+  it('given name in use should not create, expect 409', async () => {
+    const recommendationData =
+      recommendationFactory.createRecommendationData('name');
+    await recommendationFactory.createRecommendation(recommendationData);
+
+    const response = await supertest(app)
+      .post('/recommendations')
+      .send(recommendationData);
+    expect(response.status).toBe(409);
+
+    const recommendationCreated = await prisma.recommendation.findMany({
+      where: { name: recommendationData.name },
+    });
+    expect(recommendationCreated.length).toBe(1);
+  });
+
   it('given valid body should create recommendation, expect 201', async () => {
     const recommendationData = recommendationFactory.createRecommendationData();
 
@@ -72,22 +88,35 @@ describe('create music recommendation tests', () => {
     expect(recommendationCreated.youtubeLink).toBe(
       recommendationData.youtubeLink
     );
+    expect(recommendationCreated.score).toBe(0);
+  });
+});
+
+describe('upvoting recommendation tests', () => {
+  it('given a non existing recommendation id should return error, expect 404', async () => {
+    const response = await supertest(app).post('/recommendations/0/upvote');
+    expect(response.status).toBe(404);
   });
 
-  it('given name in use should not create, expect 409', async () => {
-    const recommendationData =
-      recommendationFactory.createRecommendationData('name');
-    await recommendationFactory.createRecommendation(recommendationData);
+  it('given a invalid recommendation id should return error, expect 422', async () => {
+    const response = await supertest(app).post(
+      '/recommendations/invalid/upvote'
+    );
+    expect(response.status).toBe(422);
+  });
 
-    const response = await supertest(app)
-      .post('/recommendations')
-      .send(recommendationData);
-    expect(response.status).toBe(409);
+  it('given a valid recommendation id should upvote, expect 200', async () => {
+    const recommendation = await recommendationFactory.createRecommendation();
 
-    const recommendationCreated = await prisma.recommendation.findMany({
-      where: { name: recommendationData.name },
+    const response = await supertest(app).post(
+      `/recommendations/${recommendation.id}/upvote`
+    );
+    expect(response.status).toBe(200);
+
+    const recommendationUpvoted = await prisma.recommendation.findFirst({
+      where: { id: recommendation.id },
     });
-    expect(recommendationCreated.length).toBe(1);
+    expect(recommendationUpvoted.score).toBe(1);
   });
 });
 
