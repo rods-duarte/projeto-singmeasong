1a2ed06aef401897733094a02438fc7e280080af
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-07-30 00:37:51 -0300

    Feat(recommendationService): add get, get random and get top unit tests

--- back-end/tests/unit/recommendationService.test.ts
+++ back-end/tests/unit/recommendationService.test.ts
@@ -2,11 +2,10 @@ import { jest } from '@jest/globals'; //eslint-disable-line
 import { Recommendation } from '@prisma/client';
 import { recommendationService } from '../../src/services/recommendationsService.js';
 import { recommendationRepository } from '../../src/repositories/recommendationRepository.js';
-import scenario from '../factories/scenarioFactory.js';
 import { conflictError, notFoundError } from '../../src/utils/errorUtils.js';
 
-beforeEach(async () => {
-  await scenario.clearRecommendations();
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
 describe('recommendation service unit tests', () => {
@@ -31,6 +30,78 @@ describe('recommendation service unit tests', () => {
 
       const promise = recommendationService.getById(0);
       expect(promise).rejects.toEqual(notFoundError());
+    });
+  });
+
+  describe('get tests', () => {
+    it('should call repository', async () => {
+      jest
+        .spyOn(recommendationRepository, 'findAll')
+        .mockImplementationOnce(null);
+
+      await recommendationService.get();
+      expect(recommendationRepository.findAll).toBeCalled();
+    });
+  });
+
+  describe('get random tests', () => {
+    it('given a random number < 0.7 should get recommendations with score greater than 10', async () => {
+      const recommendation: Recommendation = {
+        id: 1,
+        name: 'aaa',
+        score: 10,
+        youtubeLink: 'aaa',
+      };
+      jest.spyOn(Math, 'random').mockImplementationOnce(() => 0.69);
+      jest
+        .spyOn(recommendationRepository, 'findAll')
+        .mockResolvedValueOnce([recommendation]);
+
+      await recommendationService.getRandom();
+      expect(recommendationRepository.findAll).toBeCalledWith({
+        score: 10,
+        scoreFilter: 'gt',
+      });
+    });
+
+    it('given no recommendation registered should throw not found error', async () => {
+      jest.spyOn(Math, 'random').mockImplementationOnce(() => 0.69);
+      jest.spyOn(recommendationRepository, 'findAll').mockResolvedValueOnce([]);
+
+      const promise = recommendationService.getRandom();
+      expect(promise).rejects.toEqual(notFoundError());
+      expect(recommendationRepository.findAll).toBeCalledTimes(1);
+    });
+
+    it('given a random number >= 0.7 should get recommendations with score less then 10', async () => {
+      const recommendation: Recommendation = {
+        id: 1,
+        name: 'aaa',
+        score: -5,
+        youtubeLink: 'aaa',
+      };
+      jest.spyOn(Math, 'random').mockImplementationOnce(() => 0.7);
+      jest
+        .spyOn(recommendationRepository, 'findAll')
+        .mockResolvedValueOnce([recommendation]);
+
+      await recommendationService.getRandom();
+      expect(recommendationRepository.findAll).toBeCalledWith({
+        score: 10,
+        scoreFilter: 'lte',
+      });
+    });
+  });
+
+  describe('get top tests', () => {
+    it('should call repository', async () => {
+      jest
+        .spyOn(recommendationRepository, 'getAmountByScore')
+        .mockImplementationOnce(null);
+
+      const amount = 10;
+      await recommendationService.getTop(amount);
+      expect(recommendationRepository.getAmountByScore).toBeCalled();
     });
   });
 
@@ -64,8 +135,4 @@ describe('recommendation service unit tests', () => {
       );
     });
   });
-});
-
-afterAll(async () => {
-  await scenario.clearRecommendations();
 });
