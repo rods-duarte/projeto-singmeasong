5e6618c19fc82b64ba2cc06f0a37292e827b12b6
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-07-30 02:45:40 -0300

    Feat: add utils

--- back-end/tests/testUtils.ts
+++ back-end/tests/testUtils.ts
@@ -1,3 +1,14 @@
+import { Recommendation } from '@prisma/client';
+
 export function getRandomNumber(min: number, max: number) {
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+export function isRecommendationsSortedByOrder(arr: Recommendation[]) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].score > arr[i - 1].score) {
+      return false;
+    }
+  }
+  return true;
 }
